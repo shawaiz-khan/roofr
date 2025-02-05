1e7bd15b0dff627abbd41a340dcb5ef51e14a308
Shawaiz Khan <shawaizkhan.biz@gmail.com>
2025-02-05 07:18:47 +0000

    add: Animated the rating blocks

--- src/components/RatingBlock.tsx
+++ src/components/RatingBlock.tsx
@@ -5,7 +5,7 @@ const RatingBlock: React.FC<RatingBlockProps> = (props) => {
     const { count, text } = props;
 
     return (
-        <div className='bg-black-secondary border border-black-tertiary px-3 py-5 md:px-7 flex flex-col justify-center items-center rounded-md gap-1'>
+        <div className='bg-black-secondary border border-black-tertiary px-3 py-5 md:px-7 flex flex-col justify-center items-center rounded-md gap-1 hover:-translate-y-2 duration-300 ease-in cursor-pointer'>
             <h1 className='font-semibold text-2xl'>{count}+</h1>
             <span className='text-gray-quaternary text-xs text-center'>{text}</span>
         </div>
