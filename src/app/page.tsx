63f6d597c77bc845edf6f6f75b4f5c3b311e793c
Shawaiz Khan <shawaizkhan.biz@gmail.com>
2025-02-05 05:30:51 +0000

    add: Added the round animated graphics on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,22 @@
+import Image from "next/image";
 import React from "react";
+import heroImg from '@/assets/images/hero.jpg';
+import RoundTextSvg from "@/components/ui/RoundTextSvg";
 
 const Home: React.FC = () => {
   return (
-    <main className="p-5 min-h-screen bg-black-primary">Hello World</main>
+    <main className="p-5 min-h-screen bg-black-primary text-text-light">
+      <div className="relative h-80">
+        <Image
+          src={heroImg}
+          alt="Hero Image"
+          fill
+          priority
+          className="absolute inset-0 object-cover overflow-hidden rounded-xl"
+        />
+        <RoundTextSvg className="absolute bottom-0 -translate-x-[15%] translate-y-1/4" />
+      </div>
+    </main>
   );
 }
 
