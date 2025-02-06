1f779d626f30110667a18c1c88a8790b453a2d70
Shawaiz Khan <shawaizkhan.biz@gmail.com>
2025-02-06 08:27:51 +0000

    add: Footer socials made responsive

--- src/components/FooterSocials.tsx
+++ src/components/FooterSocials.tsx
@@ -5,27 +5,27 @@ const FooterSocials: React.FC = () => {
     const newDate = new Date().getFullYear().toString();
 
     return (
-        <div className="bg-black-secondary p-5 max-h-48 flex flex-col items-center justify-center gap-5">
+        <div className="bg-black-secondary py-5 md:py-3 px-10 max-h-48 flex flex-col md:flex-row-reverse items-center justify-center md:justify-between gap-5">
             <div className='flex items-center justify-center gap-3'>
-                <Link href={'/'} className="relative w-14 h-14 flex items-center justify-center hover:scale-105 duration-300 ease-in">
-                    <span className="bg-black-primary absolute inset-0 m-auto h-14 w-14 rounded-full"></span>
+                <Link href={'/'} className="relative w-14 h-14 md:w-12 md:h-12 flex items-center justify-center hover:scale-105 duration-300 ease-in">
+                    <span className="bg-black-primary absolute inset-0 m-auto h-14 w-14 md:w-12 md:h-12 rounded-full"></span>
                     <Instagram className="relative z-10 text-text-light" />
                 </Link>
-                <Link href={'/'} className="relative w-14 h-14 flex items-center justify-center hover:scale-105 duration-300 ease-in">
-                    <span className="bg-black-primary absolute inset-0 m-auto h-14 w-14 rounded-full"></span>
+                <Link href={'/'} className="relative w-14 h-14 md:w-12 md:h-12 flex items-center justify-center hover:scale-105 duration-300 ease-in">
+                    <span className="bg-black-primary absolute inset-0 m-auto h-14 w-14 md:w-12 md:h-12 rounded-full"></span>
                     <Linkedin className="relative z-10 text-text-light" />
                 </Link>
-                <Link href={'/'} className="relative w-14 h-14 flex items-center justify-center hover:scale-105 duration-300 ease-in">
-                    <span className="bg-black-primary absolute inset-0 m-auto h-14 w-14 rounded-full"></span>
+                <Link href={'/'} className="relative w-14 h-14 md:w-12 md:h-12 flex items-center justify-center hover:scale-105 duration-300 ease-in">
+                    <span className="bg-black-primary absolute inset-0 m-auto h-14 w-14 md:w-12 md:h-12 rounded-full"></span>
                     <Facebook className="relative z-10 text-text-light" />
                 </Link>
-                <Link href={'/'} className="relative w-14 h-14 flex items-center justify-center hover:scale-105 duration-300 ease-in">
-                    <span className="bg-black-primary absolute inset-0 m-auto h-14 w-14 rounded-full"></span>
+                <Link href={'/'} className="relative w-14 h-14 md:w-12 md:h-12 flex items-center justify-center hover:scale-105 duration-300 ease-in">
+                    <span className="bg-black-primary absolute inset-0 m-auto h-14 w-14 md:w-12 md:h-12 rounded-full"></span>
                     <Youtube className="relative z-10 text-text-light" />
                 </Link>
             </div>
-            <div className='flex flex-col justify-center items-center text-text-light text-sm gap-2'>
-                <h1>&copy;{newDate} Roofr. All Rights Reserved.</h1>
+            <div className='flex flex-col md:flex-row justify-center items-center text-text-light text-sm gap-2'>
+                <h1>&copy; {newDate} Roofr. All Rights Reserved.</h1>
                 <Link href={'/'}><span className='cursor-pointer'>Terms & Conditions</span></Link>
             </div>
         </div>
