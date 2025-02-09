cc94066348713808b8b360320c72325dc9b11679
Shawaiz Khan <shawaizkhan.biz@gmail.com>
2025-02-09 19:26:58 +0000

    fix: Initialize the connection of db

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -3,17 +3,14 @@ import { NextRequest } from 'next/server';
 import connectDB from "@/lib/db";
 import { getUsers, loginUser } from '@/controllers/authController';
 
-connectDB();
 
 export async function POST(request: NextRequest) {
+    await connectDB();
     const body = await request.json();
     return loginUser(body);
 }
 
 export async function GET(request: NextRequest) {
+    await connectDB();
     return getUsers();
 }
-
-export const config = {
-    runtime: "nodejs",
-};
