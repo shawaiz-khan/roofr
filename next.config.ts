0e5048d8cd776cbda4ae95a116fb6e2931f588c2
Shawaiz Khan <shawaizkhan.biz@gmail.com>
2025-02-09 11:14:13 +0000

    fix: Removing timeout

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  api: {
+    responseLimit: false,
+    externalResolver: true,
+  },
 };
 
 export default nextConfig;
