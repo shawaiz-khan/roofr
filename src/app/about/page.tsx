249503341a6d5133594826c5fc88bd45d9dccfdf
Shawaiz Khan <shawaizkhan.biz@gmail.com>
2025-02-18 19:02:56 +0000

    update: Completed the about page for small devices

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,79 +3,94 @@ import abt_bg_cover from '@/assets/svg/Background Graphics.svg';
 import stars from "@/assets/svg/Stars.svg";
 import { AboutFeatureList } from '@/components/block/AboutFeaturesList';
 import AchievementBlock from '@/components/block/AchievementBlock';
+import CTA from '@/components/block/CTA';
 import ExperienceBlock from '@/components/block/ExperienceBlock';
 import RatingBlock from '@/components/block/RatingBlock';
+import TeamBlock from '@/components/block/TeamBlock';
 import Image from 'next/image';
 
 const About: React.FC = () => {
     return (
-        <main className="min-h-screen bg-black-primary text-text-light flex flex-col items-center px-5 py-10 gap-10">
-            {/* About Hero  */}
-            <div className='border border-black-tertiary rounded-xl relative overflow-hidden'>
-                <Image
-                    src={abt_cover}
-                    alt='About Roofr'
-                    className='relative'
-                />
+        <main className="min-h-screen bg-black-primary text-text-light flex flex-col items-center gap-10 md:gap-0">
+            <div className='p-5 md:p-0 flex flex-col gap-10 md:gap-0'>
+                {/* About Hero  */}
+                <div className='border border-black-tertiary rounded-xl relative overflow-hidden'>
+                    <Image
+                        src={abt_cover}
+                        alt='About Roofr'
+                        className='relative'
+                    />
 
-                <Image
-                    src={abt_bg_cover}
-                    alt='About Roofr'
-                    layout='fill'
-                    className='absolute inset-0 object-cover opacity-60'
-                />
-            </div>
-            {/* Journey  */}
-            <div className="flex flex-col gap-2">
-                <Image src={stars} alt="What our client says" />
-                <h1 className="text-2xl">Our Journey</h1>
-                <p className="text-sm text-gray-quaternary md:w-[90%]">
-                    Our story is one of continuous growth and evolution. We started as a small team with big dreams,
-                    determined to create a real estate platform that transcended the ordinary. Over the years, we have
-                    expanded our reach, forged valuable partnerships, and gained the trust of countless clients.
-                </p>
-            </div>
-            {/* Ratings  */}
-            <div className="grid grid-cols-2 md:grid-cols-3 gap-3 w-full">
-                <RatingBlock count={200} text="Happy Customers" />
-                <RatingBlock count="10k" text="Properties For Clients" />
-                <div className="col-span-2 md:col-span-1">
-                    <RatingBlock count={16} text="Years of Experience" />
+                    <Image
+                        src={abt_bg_cover}
+                        alt='About Roofr'
+                        layout='fill'
+                        className='absolute inset-0 object-cover opacity-60'
+                    />
                 </div>
+                {/* Journey  */}
+                <div className="flex flex-col gap-2">
+                    <Image src={stars} alt="What our client says" />
+                    <h1 className="text-2xl">Our Journey</h1>
+                    <p className="text-sm text-gray-quaternary md:w-[90%]">
+                        Our story is one of continuous growth and evolution. We started as a small team with big dreams,
+                        determined to create a real estate platform that transcended the ordinary. Over the years, we have
+                        expanded our reach, forged valuable partnerships, and gained the trust of countless clients.
+                    </p>
+                </div>
+                {/* Ratings  */}
+                <div className="grid grid-cols-2 md:grid-cols-3 gap-3 w-full">
+                    <RatingBlock count={200} text="Happy Customers" />
+                    <RatingBlock count="10k" text="Properties For Clients" />
+                    <div className="col-span-2 md:col-span-1">
+                        <RatingBlock count={16} text="Years of Experience" />
+                    </div>
+                </div>
+                {/* Values  */}
+                <div className="flex flex-col gap-2">
+                    <Image src={stars} alt="What our client says" />
+                    <h1 className="text-2xl">Our Values</h1>
+                    <p className="text-sm text-gray-quaternary md:w-[90%]">
+                        Our story is one of continuous growth and evolution. We started as a small team with big dreams,
+                        determined to create a real estate platform that transcended the ordinary.
+                    </p>
+                </div>
+                <div className='bg-black-primary flex flex-col p-5 gap-3 rounded-md shadow-[0_0_10px_#262626]'>
+                    <AboutFeatureList />
+                </div>
+                {/* Achievements  */}
+                <div className="flex flex-col gap-2">
+                    <Image src={stars} alt="What our client says" />
+                    <h1 className="text-2xl">Our Achievements</h1>
+                    <p className="text-sm text-gray-quaternary md:w-[90%]">
+                        Our story is one of continuous growth and evolution. We started as a small team
+                        with big dreams, determined to create a real estate platform that transcended the ordinary.
+                    </p>
+                </div>
+                <AchievementBlock />
+                {/* Experience  */}
+                <div className="flex flex-col gap-2">
+                    <Image src={stars} alt="What our client says" />
+                    <h1 className="text-2xl">Navigating the Roofr Experience</h1>
+                    <p className="text-sm text-gray-quaternary md:w-[90%]">
+                        At Roofr, we have designed a straightforward process to help you find and purchase your dream property with ease.
+                        Here is a step-by-step guide to how it all works.
+                    </p>
+                </div>
+                <ExperienceBlock />
+                {/* Developer  */}
+                <div className="flex flex-col gap-2">
+                    <Image src={stars} alt="What our client says" />
+                    <h1 className="text-2xl">Meet the Developer</h1>
+                    <p className="text-sm text-gray-quaternary md:w-[90%]">
+                        At Roofr, our success is driven by the dedication and expertise of our team. Get to know the people behind our
+                        mission to make your real estate dreams a reality.
+                    </p>
+                </div>
+                <TeamBlock />
             </div>
-            {/* Values  */}
-            <div className="flex flex-col gap-2">
-                <Image src={stars} alt="What our client says" />
-                <h1 className="text-2xl">Our Values</h1>
-                <p className="text-sm text-gray-quaternary md:w-[90%]">
-                    Our story is one of continuous growth and evolution. We started as a small team with big dreams,
-                    determined to create a real estate platform that transcended the ordinary.
-                </p>
-            </div>
-            {/* Features  */}
-            <div className='bg-black-primary flex flex-col p-5 gap-3 rounded-md shadow-[0_0_10px_#262626]'>
-                <AboutFeatureList />
-            </div>
-            {/* Achievements  */}
-            <div className="flex flex-col gap-2">
-                <Image src={stars} alt="What our client says" />
-                <h1 className="text-2xl">Our Achievements</h1>
-                <p className="text-sm text-gray-quaternary md:w-[90%]">
-                    Our story is one of continuous growth and evolution. We started as a small team
-                    with big dreams, determined to create a real estate platform that transcended the ordinary.
-                </p>
-            </div>
-            <AchievementBlock />
-            {/* Experience  */}
-            <div className="flex flex-col gap-2">
-                <Image src={stars} alt="What our client says" />
-                <h1 className="text-2xl">Navigating the Roofr Experience</h1>
-                <p className="text-sm text-gray-quaternary md:w-[90%]">
-                    At Roofr, we have designed a straightforward process to help you find and purchase your dream property with ease.
-                    Here is a step-by-step guide to how it all works.
-                </p>
-            </div>
-            <ExperienceBlock />
+            {/* CTA  */}
+            <CTA />
         </main>
     );
 }
--- src/components/block/AchievementBlock.tsx
+++ src/components/block/AchievementBlock.tsx
@@ -2,10 +2,10 @@ import { Achievements } from "@/data/achievements";
 
 const AchievementBlock: React.FC = () => {
     return (
-        <div className="w-full grid grid-cols-1 gap-3">
+        <div className="w-full grid grid-cols-1 gap-5">
             {Achievements.map((achievement) => (
                 <div
-                    className="bg-black-secondary border border-black-tertiary px-5 py-7 flex flex-col rounded-md gap-3"
+                    className="bg-black-primary px-5 py-7 flex flex-col rounded-md gap-3 shadow-[0_0_10px_#262626]"
                     key={achievement.text}
                 >
                     <h1 className="text-xl font-semibold">{achievement.text}</h1>
