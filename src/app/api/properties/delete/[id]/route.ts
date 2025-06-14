408aa10927b6b71bd11a0187940ddb6b6ea3893a
Shawaiz Khan <shawaizkhan.biz@gmail.com>
2025-06-14 05:39:23 +0100

    add: Deployment issue pt.7

--- src/app/api/properties/delete/[id]/route.ts
+++ src/app/api/properties/delete/[id]/route.ts
@@ -2,16 +2,16 @@ import { NextRequest } from "next/server";
 import connectDB from "@/lib/db";
 import { Delete_Property } from "@/controllers/property.controller";
 
-connectDB();
+await connectDB();
 
 export async function POST(
     req: NextRequest,
     context: { params: { id: string } }
 ) {
-    const { id } = context.params;
+    const id = context.params.id;
 
     if (!id) {
-        return new Response(JSON.stringify({ message: "Missing ID" }), {
+        return new Response(JSON.stringify({ message: "Missing property ID" }), {
             status: 400,
             headers: { "Content-Type": "application/json" },
         });
