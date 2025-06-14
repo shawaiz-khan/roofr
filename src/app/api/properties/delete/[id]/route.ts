3c093591fce0c7b2aefc9f176cf562081d3a0991
Shawaiz Khan <shawaizkhan.biz@gmail.com>
2025-06-14 04:59:46 +0100

    feat: Delete route fixed again pt.3

--- src/app/api/properties/delete/[id]/route.ts
+++ src/app/api/properties/delete/[id]/route.ts
@@ -1,11 +1,14 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { NextRequest } from "next/server";
 import connectDB from "@/lib/db";
 import { Delete_Property } from "@/controllers/property.controller";
 
 connectDB();
 
-export async function POST(req: NextRequest, { params }: { params: { id: string } }) {
+export async function POST(
+    { params }: { params: { id: string } },
+    req: NextRequest
+) {
     const { id } = params;
-
     return await Delete_Property(id);
 }
